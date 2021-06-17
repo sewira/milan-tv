8bb0d21ed37b32cf29b75cfee9cbd7f10f647b30
yeyen tri utami <yeyentriutami13@gmail.com>
2021-06-17 13:13:17 +0700

    yayaaaa

--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -1,11 +1,83 @@
-import React from 'react'
+import React from 'react';
+import './characters.css';
+
+// const karak = ({}) =>{
+    // return (
+        // <div className ="karakter">
+            // {/* <img src={} alt=""/> */}
+            // {/* <h4>{nama}</h4> */}
+        // {/* </div> */}
+    // )
+// }
+// 
 
 function Characters() {
     return (
-        <div>
-            test characters
+        <div className="karakter" >
+           <div className="karakter-item">
+           <img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+           <h4 className="nama">Paolo Maldini</h4>
+           </div>
+           
+           <div className="karakter-item">
+  <img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Andriy_Shevchenko_2017.jpg/220px-Andriy_Shevchenko_2017.jpg"/>
+  <h4 className="nama">Paolo Maldini</h4>
+  </div>
+
+  <div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+
+<div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+
+<div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+
+
+<div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+
+<div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+
+<div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+
+<div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+
+<div className="karakter-item">
+<img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/>
+<h4 className="nama">Paolo Maldini</h4>
+</div>
+  
+  
+           
+            {/* <img className="gambar"  src="https://upload.wikimedia.org/wikipedia/commons/1/15/Maldini2008.JPG"/> */}
+            {/* <h4 className="nama"> Paolo Maldini</h4> */}
+            {/* <img className="gambar" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Andriy_Shevchenko_2017.jpg/220px-Andriy_Shevchenko_2017.jpg"/> */}
+            {/* <h4 className="nama"> Andriy Shevcenko</h4> */}
+            {/* <img className="gambar" src="https://www.scoutedftbl.com/wp-content/uploads/2020/12/manuel-locatelli.jpg"/> */}
+            {/* <h4 className="nama"> Manuel locatelli</h4> */}
+            {/* <img className="gambar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiXxTSwUjzmQiLDc23TMb-J8q9bH0co74U1Q&usqp=CAU"/> */}
+            {/* <h4 className="nama"> Andrea Pirlo</h4> */}
         </div>
+
     )
-}
+};
 
 export default Characters
